8182c57580a586d1c636126b35b80cb6d8f5403a
BraylexDev <perezbrayan1104@gmail.com>
2026-07-03 15:11:47 -0500

    chang api prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://backendbufete-docker.onrender.com/api',
 };
